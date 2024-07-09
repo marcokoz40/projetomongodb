20be94c70d63610cc390d4a54329e4baf56a95eb
Marco Junior <silva241277@gmail.com>
2024-07-09 15:51:30 -0300

    Projeto MongoDB Criado

---
+++ src/main/java/com/example/marcojr/ProjetomongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.marcojr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marcojr/ProjetomongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marcojr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
