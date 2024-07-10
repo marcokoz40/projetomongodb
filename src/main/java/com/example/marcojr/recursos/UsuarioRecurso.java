3bafcd1c689f95b6ea89480c38f16c4d7e763fcc
Marco Junior <silva241277@gmail.com>
2024-07-10 12:26:08 -0300

    Atualização e Exclusão de Usuarios

--- src/main/java/com/example/marcojr/recursos/UsuarioRecurso.java
+++ src/main/java/com/example/marcojr/recursos/UsuarioRecurso.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,23 +23,23 @@ import com.example.marcojr.servicos.UsuarioServico;
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioRecurso {
-	
+
 	@Autowired
 	private UsuarioServico servico;
-	
+
 	@GetMapping
 	public ResponseEntity<List<UsuarioDTO>> findAll() {
 		List<Usuario> lista = servico.findAll();
 		List<UsuarioDTO> listaDto = lista.stream().map(x -> new UsuarioDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listaDto);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UsuarioDTO> findById(@PathVariable String id) {
 		Usuario obj = servico.findById(id);
 		return ResponseEntity.ok().body(new UsuarioDTO(obj));
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody UsuarioDTO objDto) {
 		Usuario obj = servico.fromDTO(objDto);
@@ -45,9 +47,19 @@ public class UsuarioRecurso {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	
-	
-	
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		servico.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UsuarioDTO objDto, @PathVariable String id) {
+		Usuario obj = servico.fromDTO(objDto);
+		obj.setId(id);
+		obj = servico.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 
 }
--- src/main/java/com/example/marcojr/servicos/UsuarioServico.java
+++ src/main/java/com/example/marcojr/servicos/UsuarioServico.java
@@ -13,23 +13,40 @@ import com.example.marcojr.servicos.excecoes.ObjectNotFoundException;
 
 @Service
 public class UsuarioServico {
-	
+
 	@Autowired
 	private UsuarioRepositorio repositorio;
-	
+
 	public List<Usuario> findAll() {
 		return repositorio.findAll();
 	}
-	
+
 	public Usuario findById(String id) {
 		Optional<Usuario> usuario = repositorio.findById(id);
-		return usuario.orElseThrow(() -> new ObjectNotFoundException(id)); 
+		return usuario.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public Usuario insert(Usuario obj) {
 		return repositorio.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repositorio.deleteById(id);
+	}
+
+	public Usuario update(Usuario obj) {
+		Usuario newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repositorio.save(newObj);
+	}
+
+	private void updateData(Usuario newObj, Usuario obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+
+	}
+
 	public Usuario fromDTO(UsuarioDTO objDto) {
 		return new Usuario(objDto.getId(), objDto.getNome(), objDto.getEmail());
 	}
