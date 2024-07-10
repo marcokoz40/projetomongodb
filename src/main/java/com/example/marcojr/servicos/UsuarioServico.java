377bbf7d30d948c22ba2c077643545f1b03608e1
Marco Junior <silva241277@gmail.com>
2024-07-10 11:05:40 -0300

    Inserção de Usuario

--- src/main/java/com/example/marcojr/servicos/UsuarioServico.java
+++ src/main/java/com/example/marcojr/servicos/UsuarioServico.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.marcojr.dominios.Usuario;
+import com.example.marcojr.dto.UsuarioDTO;
 import com.example.marcojr.repositorios.UsuarioRepositorio;
 import com.example.marcojr.servicos.excecoes.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UsuarioServico {
 	public Usuario findById(String id) {
 		Optional<Usuario> usuario = repositorio.findById(id);
 		return usuario.orElseThrow(() -> new ObjectNotFoundException(id)); 
+	}
+	
+	public Usuario insert(Usuario obj) {
+		return repositorio.insert(obj);
+	}
+	
+	public Usuario fromDTO(UsuarioDTO objDto) {
+		return new Usuario(objDto.getId(), objDto.getNome(), objDto.getEmail());
 	}
 
 }
